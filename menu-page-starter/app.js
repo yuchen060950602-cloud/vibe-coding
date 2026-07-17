b9c6d98c2997554a3778ff83048d1fdbe052b0f8
yuchen060950602-cloud <yuchen0609.506.02@gmail.com>
2026-07-17 09:30:28 +0800

    Delete menu-page-starter directory

--- menu-page-starter/app.js
+++
@@ -1,7 +0,0 @@
-const buttons = document.querySelectorAll("[data-item]");
-
-buttons.forEach((button) => {
-  button.addEventListener("click", () => {
-    window.alert(`已將「${button.dataset.item}」加入購物車！`);
-  });
-});
